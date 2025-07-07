704302e6b212bac4b7eaceabd836fe8fb3673473
NjabuloJ <fanajb65@gmail.com>
2025-07-08 00:09:26 +0200

    Create no.js

---
+++ plugins/no.js
@@ -0,0 +1,80 @@
+import config from '../../config.cjs';
+import pkg, { prepareWAMessageMedia } from '@whiskeysockets/baileys';
+const { generateWAMessageFromContent, proto } = pkg;
+
+const alwaysonlineCommand = async (m, Matrix) => {
+  const botNumber = await Matrix.decodeJid(Matrix.user.id);
+  const isCreator = [botNumber, config.OWNER_NUMBER + '@s.whatsapp.net'].includes(m.sender);
+  const prefixMatch = m.body.match(/^[\\/!#.]/);
+  const prefix = prefixMatch ? prefixMatch[0] : '/';
+  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+  const text = m.body.slice(prefix.length + cmd.length).trim().toLowerCase();
+
+  if (cmd === 'online') {
+    if (!isCreator) return m.reply("* THIS IS AN OWNER COMMAND*");
+
+    let responseMessage;
+    if (text === 'on') {
+      config.ALWAYS_ONLINE = true;
+      responseMessage = "Always Online has been enabled.";
+    } else if (text === 'off') {
+      config.ALWAYS_ONLINE = false;
+      responseMessage = "Always Online has been disabled.";
+    } else {
+      responseMessage = "Usage:\n- `alwaysonline on`: Enable Always Online\n- `alwaysonline off`: Disable Always Online";
+    }
+
+    const buttons = [
+      {
+        "name": "cta_url",
+        "buttonParamsJson": JSON.stringify({
+          display_text: "Join Our Community",
+          url: `https://whatsapp.com/channel/0029VaAkETLLY6d8qhLmZt2v`
+        })
+      }
+    ];
+
+    const msg = generateWAMessageFromContent(m.from, {
+      viewOnceMessage: {
+        message: {
+          messageContextInfo: {
+            deviceListMetadata: {},
+            deviceListMetadataVersion: 2
+          },
+          interactiveMessage: proto.Message.InteractiveMessage.create({
+            body: proto.Message.InteractiveMessage.Body.create({
+              text: responseMessage
+            }),
+            footer: proto.Message.InteractiveMessage.Footer.create({
+              text: "Always Online"
+            }),
+            header: proto.Message.InteractiveMessage.Header.create({
+              title: "",
+              gifPlayback: true,
+              subtitle: "",
+              hasMediaAttachment: false
+            }),
+            nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+              buttons
+            }),
+          }),
+        },
+      },
+    }, {});
+
+    try {
+      await Matrix.relayMessage(msg.key.remoteJid, msg.message, {
+        messageId: msg.key.id
+      });
+    } catch (error) {
+      console.error("Error processing your request:", error);
+      await Matrix.sendMessage(m.from, {
+        text: 'Error processing your request.'
+      }, {
+        quoted: m
+      });
+    }
+  }
+};
+
+export default alwaysonlineCommand;
