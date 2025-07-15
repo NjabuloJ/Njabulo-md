02f61f4fad270e9b64bb5dd997932d196798d877
NjabuloJ <fanajb65@gmail.com>
2025-07-15 14:38:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import {
   fetchLatestBaileysVersion,
   DisconnectReason,
   useMultiFileAuthState,
-} from "baileys-pro": "github:nstar-y/bail";
+} from "baileys-pro";
 import { Handler, Callupdate, GroupUpdate } from "./data/index.js";
 import express from "express";
 import pino from "pino";
