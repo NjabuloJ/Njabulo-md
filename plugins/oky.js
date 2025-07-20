360ab7a06a8bf899e7247fde856b8dd9db0700c0
NjabuloJ <fanajb65@gmail.com>
2025-07-20 10:40:35 +0200

    Update oky.js

--- plugins/oky.js
+++ plugins/oky.js
@@ -38,7 +38,7 @@ const alive = async (m, Matrix) => {
 
     await m.React(textEmoji);
 
-    const message = ${toFancyFont("aira")}: ${timeString}!;
+    const message = `${toFancyFont("aira")}: ${timeString}!`;
     const messageOptions = {
      viewOnce: true,
       buttons: [
